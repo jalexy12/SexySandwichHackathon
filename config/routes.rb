49e7a73a352093a2f8820d915586a87e1902a99e
Josh <jalexy12@gmail.com>
2016-02-20 02:54:59 -0500

    Added sandwich and user models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/' => 'static_pages#home'
   get '/oauth/callbacks' => "callbacks#instagram"
   # The priority is based upon order of creation: first created -> highest priority.
