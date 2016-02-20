15b706571a05e1c9fa7a5a2d7eef962e713e0044
Josh <jalexy12@gmail.com>
2016-02-19 23:47:25 -0500

    Created callbacks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/' => "callbacks#instagram"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
