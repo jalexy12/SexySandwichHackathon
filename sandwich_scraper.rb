9e2e5f2601a953c75d39185bfc89fa189eef2eeb
Josh <jalexy12@gmail.com>
2016-02-20 17:51:18 -0500

    Added quick sandwich scraper script

--- sandwich_scraper.rb
+++ sandwich_scraper.rb
@@ -2,9 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 
 sandwich_page = Nokogiri::HTML(open("https://www.thrillist.com/food/nation/national-sandwich-day"))
-sandwiches = sandwich_page.css(".caption > strong").map {| sandwich | 
-	sandwich_text = sandwich.text.split(" ")[1..-1].join(" ")
-}.join("\n")
+# sandwiches = sandwich_page.css(".caption > strong").map { | sandwich | 
+# 	sandwich_text = sandwich.text.split(" ")[1..-1].join(" ")
+# }.join("\n")
 
-IO.write("./public/sandwich_list.txt", sandwiches)
+# IO.write("./public/sandwich_list.txt", sandwiches)
 
+sandwich_images = sandwich_page.css(".desktop > .img").map { | sandwich |
+	puts sandwich.styles
+}
